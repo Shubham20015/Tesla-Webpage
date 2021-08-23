d7ae21ec33cb424b31602da9f0ea0fde8d8f8652
Shubham <Shubham20015>
2021-08-23 22:43:51 +0530

    Adding Project to Github

---
+++ src/components/Header.js
@@ -0,0 +1,166 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from "react-redux";
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [y, setY] = useState(0);
+
+  const handleNavigation = (e) => {
+    const window = e.currentTarget;
+    setY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => handleNavigation(e));
+  }, []);
+
+  const cars = useSelector(selectCars);
+
+  return (
+    <Container isScroll={y}>
+      <a href="/">
+        <img src="/images/logo.svg" alt="TESLA Logo" />
+      </a>
+      <Menu>
+        {cars &&
+          cars.map((car, index) => (
+            <a href="/" key={index}>
+              {car}
+            </a>
+          ))}
+      </Menu>
+
+      <RightMenu>
+        <a href="/">Shop</a>
+        <a href="/">Tesla Account</a>
+        <CustomMenu onClick={() => setIsMenuOpen(true)} />
+      </RightMenu>
+
+      <BurgerNav show={isMenuOpen}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setIsMenuOpen(false)} />
+        </CloseWrapper>
+        {cars &&
+          cars.map((car, index) => (
+            <li key={index}>
+              <a href="/">{car}</a>
+            </li>
+          ))}
+        <li>
+          <a href="/">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="/">Used Inventory</a>
+        </li>
+        <li>
+          <a href="/">Trade In</a>
+        </li>
+        <li>
+          <a href="/">Cybertruck</a>
+        </li>
+        <li>
+          <a href="/">Roadaster</a>
+        </li>
+        <li>
+          <a href="/">Charging</a>
+        </li>
+        <li>
+          <a href="/">Powerwall</a>
+        </li>
+        <li>
+          <a href="/">Utilities</a>
+        </li>
+        <li>
+          <a href="/">Test Drive</a>
+        </li>
+      </BurgerNav>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  background: ${(props) => (props.isScroll > 50 ? "white" : "none")};
+  transition: background 300ms ease-in-out;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 270px;
+  list-style: none;
+  padding: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  transform: ${(props) => (props.show ? " translateX(0)" : "translateX(100%)")};
+  transition: transform 300ms ease-in-out;
+
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+
+export default Header;
